50fe097fad2c5173ccd7497ad040aef3a43d830a
Jones40 <john.olivier87@gmail.com>
2021-08-26 11:03:31 +0200

    update

--- 365-Alert Laravel/routes/web.php
+++ 365-Alert Laravel/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AlertController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +15,69 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route to the about page
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Route to the admin page
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
+
+// Route to the Alert Details page
+Route::get('/alert-details', function () {
+    return view('alert-details');
+})->name('alert-details');
+
+// Route to the Alerts page
+Route::get('/alerts', function () {
+    return view('alerts');
+})->name('alerts');
+
+Route::post('/alerts/{id}', [AlertController::class, 'alert-show'])->name('alert-show');
+
+// Route to the Contact page
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::post('/contact', [UserController::class, 'contact-form'])->name('contact-submit');
+
+// Route to the Home page
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Route to the login page
+Route::get('/login', [UserController::class, 'userEdit'])->name('login');
 
-require __DIR__.'/auth.php';
+Route::post('/login', [UserController::class, 'login'])->name('login');
+
+// Route to the procedures
+Route::get('/procedures', function () {
+    return view('procedures');
+})->name('procedures');
+
+// Route to the Register
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+//Route::get('/register', [UserController::class, 'create'])->name('register');
+
+//Route::post('/register', [UserController::class, 'store'])->name('register');
+
+
+
+// Route to the terms and conditions
+Route::get('terms', function () {
+    return view('terms');
+})->name('terms');
+
+// Route to the User Control panel view
+Route::get('user', function () {
+    return view('user');
+})->name('user');
+
+Route::post('user', [UserController::class, 'modification-submit'])->name('modification-submit');
