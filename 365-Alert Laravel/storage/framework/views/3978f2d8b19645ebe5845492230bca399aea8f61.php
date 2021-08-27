f07ff4e9a5dda672285e6dc6ffa3ad3bd9243aa8
DiogoJCA <diogocordasantao@gmail.com>
2021-08-27 14:58:30 +0200

    commit

--- 365-Alert Laravel/storage/framework/views/3978f2d8b19645ebe5845492230bca399aea8f61.php
+++ 365-Alert Laravel/storage/framework/views/3978f2d8b19645ebe5845492230bca399aea8f61.php
@@ -8,20 +8,56 @@
 <?php $__env->startSection('css'); ?>
     <link rel="stylesheet" href="<?php echo e(URL::asset('css/register.css')); ?>">
 <?php $__env->stopSection(); ?>
+<body>
+
 
 
 <?php $__env->startSection('content'); ?>
-    
+<h3>Register</h3>
+
+
+<?php if($errors->any()): ?>
+    <div class="alert alert-danger">
+        <ul>
+            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <li><?php echo e($error); ?></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+    </div>
+<?php endif; ?>
+<form action="" method="post" id="register" >
+    <!-- Security token for Laravel : Mandatory in forms -->
+    <?php echo csrf_field(); ?>
+    <input type="text" name="username" placeholder="Username"><br>
+    <input type="email" name="email" placeholder="Email"><br>
+    <input type="password" name="password" placeholder="Password"><br>
+    <input type="password" name="password_confirmation" placeholder="Confirm password"><br>
+    <input type="number" name="contact" placeholder="Phone number"><br>
+
+    <div id="checkbox">
+    <input type="checkbox" name="checkbox" value="check" id="agree" />
+    <p> I have read and accept the <span><a href="terms"> Terms and Conditions</a></span></p>
+    </div>
+    <input type="submit" value="Register" id="submit">
+
+</form>
+<div id="log">
+    <p>Already a member?<span><a href="login">Login Here  </a></span></p>
+</div>
 <?php $__env->stopSection(); ?>
+
+</body>
+
 
 
 <?php $__env->startSection('footer'); ?>
-    
+
+
 <?php $__env->stopSection(); ?>
 
 
 <?php $__env->startSection('scripts'); ?>
-    
+
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.mytemplate', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\GitHub\365-Alert\365-Alert Laravel\resources\views/register.blade.php ENDPATH**/ ?>
