4acdc91f076fb0129e476658677a44849a9af866
JudeTreanor <85668506+JudeTreanor@users.noreply.github.com>
2021-08-31 09:41:14 +0200

    Css for client settings

--- 365-Alert Laravel/resources/views/client-settings.blade.php
+++ 365-Alert Laravel/resources/views/client-settings.blade.php
@@ -5,10 +5,17 @@
 @section('title', 'Client Settings')
 
 @section('css')
-    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/client-settings.css') }}">
 @endsection
 {{-- Main Content Section --}}
 @section('content')
-    <img src="{{ URL::asset('../css/logos/User_WhiteStroke_Circle.svg') }}" alt="">
-    <h3>Client Settings</h3>
+    <main>
+        <img src="{{ URL::asset('../css/logos/User_WhiteStroke_Circle.svg') }}" alt="">
+        <h1 id="client">Client Settings</h1>
+
+        <section id="settings-bar">
+
+
+        </section>
+    </main>
 @endsection
