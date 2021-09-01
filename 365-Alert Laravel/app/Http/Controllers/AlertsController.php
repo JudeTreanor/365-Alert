a034a34524d025eafed6b346f524295db992de9c
JudeTreanor <85668506+JudeTreanor@users.noreply.github.com>
2021-09-01 13:02:27 +0200

    alert stuff

--- 365-Alert Laravel/app/Http/Controllers/AlertsController.php
+++ 365-Alert Laravel/app/Http/Controllers/AlertsController.php
@@ -9,7 +9,7 @@ use GuzzleHttp\Client;
 class AlertsController extends Controller
 {
     public function getApi()
-    {   
+    {
         // Create a new client
         $alerts = new Client();
 
@@ -22,128 +22,58 @@ class AlertsController extends Controller
         // Decode the JSON file into an Object
         $responseBodys = json_decode($response->getBody());
 
-        // Looping through the Object 
-        foreach ($responseBodys as $response) {
+        // $new_array = array($responseBodys);
 
+        // var_dump($responseBodys);
+
+        // Looping through the Object 
+        $results = array();
+        foreach ($responseBodys as $response) {
             // Nested Loop to Iterate through the alert_levels array
             foreach ($response->alert_levels as $key => $level) {
-                $answer = 250;
+
                 // Conditionals to save the water levels into variables and rehuse them to check
                 // if the current water levels are at certain levels or normal
-                if ($level->name === "Cote d'alerte") {
-                
+                if ($level->name === 'Cote d’alerte') {
+
                     $redAlertLevel = $level->value;
 
-                    if ($answer >= $redAlertLevel) {
-                        echo $response->city . " is on Red Alert<br>";
-                    }
+                    if ($response->current->value >= $redAlertLevel) {
+                        $response->city . " is on Red Alert<br>";
 
+                        //return $results = [$redAlertLevel, $response->city . " is on Red Alert<br>", $orangeAlertLevel, $response->city . " is on Orange Alert<br>"];
+                    }
                 } else if ($level->name === 'Cote de préalerte') {
-                
+
                     $orangeAlertLevel = $level->value;
 
-                    if ($answer >= $orangeAlertLevel) {
-                        echo $response->city . " is on Orange Alert<br>";
+                    if ($response->current->value >= $orangeAlertLevel) {
+                        $response->city . " is on Orange Alert<br>";
+                        //  return $results = [$orangeAlertLevel, $response->city . " is on Orange Alert<br>"];
                     }
-
                 } else if ($level->name === 'Cote de vigilance') {
-                    
+
                     $yellowAlertLevel = $level->value;
-                
-                    if ($answer >= $yellowAlertLevel) {
-                        echo $response->city . " is on Yellow Alert<br>";
-                    }else {
-                        echo $response->city . " is " . $response->current->value . " cm and everything is okei<br>";
+
+                    if ($response->current->value >= $yellowAlertLevel) {
+                        $response->city . " is on Yellow Alert<br>";
+                        //   return $results = [$yellowAlertLevel, $response->city . " is on Yellow Alert<br>"];
+                    } else {
+                        // return $response->city . " is " . $response->current->value . " cm and everything is okei<br>";
                     }
-                } 
-                
+                }
+
                 if ($response->current->value <= $response->minimum->value) {
                     echo $response->city . " is dry";
                 }
+
+                echo json_encode($results, JSON_PRETTY_PRINT);
             }
         }
-            
+        return $results = [$redAlertLevel, $response->city . " is on Red Alert<br>", $orangeAlertLevel, $response->city . " is on Orange Alert<br>", $yellowAlertLevel, $response->city . " is on Yellow Alert<br>"];
+
+
 
         return view('alerts');
-
-
-        //create controller to pull from api
-        //take data from api and make alert fuintions with ifs and shit 
-
-
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Alerts  $alerts
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Alerts $alerts)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Alerts  $alerts
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Alerts $alerts)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Alerts  $alerts
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Alerts $alerts)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Alerts  $alerts
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Alerts $alerts)
-    {
-        //
     }
 }
--- 365-Alert Laravel/resources/views/alerts.blade.php
+++ 365-Alert Laravel/resources/views/alerts.blade.php
@@ -6,20 +6,58 @@
 
 {{-- CSS Link --}}
 @section('css')
-    <link rel="stylesheet" href="{{ URL::asset('css/alerts.css'); }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/alerts.css') }}">
 @endsection
 
 {{-- Main Content Section --}}
 @section('content')
-    
+
 @endsection
 
 {{-- Footer Section --}}
 @section('footer')
-    
+
 @endsection
 
 {{-- Scripts links --}}
 @section('scripts')
-    
+
 @endsection
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <div id="results_container"></div>
+</head>
+
+<body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script>
+        /* Wait for the page to be loaded/ready */
+        $(function() {
+
+            // Ajax call
+            $.ajax({
+                    url: 'AlertsController.php',
+                    method: 'get'
+                })
+                .done(function(result) {
+                    // If AJAX call worked
+                    $('#results_container').html(result);
+                })
+                .fail(function(result) {
+                    console.log('AJAX FAILED');
+                });
+        });
+    </script>
+</body>
+
+</html>
