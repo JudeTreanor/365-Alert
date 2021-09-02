fcc4b3dc54f72ea1fd93d562b1a8fc36cc23cc81
Jones40 <john.olivier87@gmail.com>
2021-09-02 16:25:06 +0200

    home update

--- 365-Alert Laravel/resources/views/home1.blade.php
+++ 365-Alert Laravel/resources/views/home1.blade.php
@@ -15,18 +15,20 @@
 
 <section>
     <img id="telefone" src="{{URL::asset('../pictures/Home_Register_Cropped.jpg')}}" alt="">
+    <p>Be alert 365 days if potential flood appears <br> </p>
+    <span id="register"> <a href="{{ route('register') }}">Sign up</a></span><br>
+    <span id="login"> <a href="{{ route('login') }}">Login</a></span>
 
-    <span id="explainSite"> Be alert 365 days if <br> potential flood appears <br> <a href="{{ route('register') }}">Sign up</a></span>
 
     <ul>
         <li>
             <img src="" alt="">
-            <img src="{{URL::asset('../pictures/Flood_Alert_Email_Main.svg')}}" id="letter" alt=""><br>
+            <img class="img" src="{{URL::asset('../pictures/Flood_Alert_Email_Main.svg')}}" id="img1" alt=""><br>
             <span>Receive an email alert <br> with possible floods</span>
-            <img src="{{URL::asset('../pictures/Check_Water_Level_Main.svg')}}" alt=""><br>
+            <img class="img" src="{{URL::asset('../pictures/Check_Water_Level_Main.svg')}}" id="img2" alt=""><br>
             <span>Check the water level <br>of any location</span>
-            <img src="{{URL::asset('../pictures/Procedures_Main.svg')}}" alt=""><br>
-            <span>Follow important safest <br>procedures</span>
+            <img class="img" src="{{URL::asset('../pictures/Procedures_Main.svg')}}"id="img3" alt=""><br>
+            <span id="procedures">Follow important safest <br>procedures</span>
 
         </li>
     </ul>
