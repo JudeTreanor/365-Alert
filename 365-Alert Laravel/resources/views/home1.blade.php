d655142cbba5376f4ab416c6cfb4caedd28445df
Jones40 <john.olivier87@gmail.com>
2021-09-02 14:10:36 +0200

    home update

--- 365-Alert Laravel/resources/views/home1.blade.php
+++ 365-Alert Laravel/resources/views/home1.blade.php
@@ -14,18 +14,19 @@
 
 
 <section>
-    <img src="{{URL::asset('../pictures/Home_Register_Cropped.jpg')}}" alt="">
+    <img id="telefone" src="{{URL::asset('../pictures/Home_Register_Cropped.jpg')}}" alt="">
+
+    <span id="explainSite"> Be alert 365 days if <br> potential flood appears <br> <a href="{{ route('register') }}">Sign up</a></span>
 
     <ul>
         <li>
             <img src="" alt="">
-            <img src="{{URL::asset('../pictures/Flood_Alert_Email_Main.svg')}}" id="phone" alt=""><br>
+            <img src="{{URL::asset('../pictures/Flood_Alert_Email_Main.svg')}}" id="letter" alt=""><br>
             <span>Receive an email alert <br> with possible floods</span>
             <img src="{{URL::asset('../pictures/Check_Water_Level_Main.svg')}}" alt=""><br>
             <span>Check the water level <br>of any location</span>
             <img src="{{URL::asset('../pictures/Procedures_Main.svg')}}" alt=""><br>
             <span>Follow important safest <br>procedures</span>
-
 
         </li>
     </ul>
