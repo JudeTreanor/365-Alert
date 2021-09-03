6dc977291407c05bdf94cec9badbd76c091b2301
DiogoJCA <diogocordasantao@gmail.com>
2021-09-03 14:57:48 +0200

    commit

--- 365-Alert Laravel/routes/web.php
+++ 365-Alert Laravel/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\AlertController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Auth\Events\PasswordReset;
--- 365-Alert Laravel/storage/framework/views/3c42a0197a812b540cd19714736a5d93fbe2c403.php
+++ 365-Alert Laravel/storage/framework/views/3c42a0197a812b540cd19714736a5d93fbe2c403.php
@@ -11,7 +11,33 @@
 
 
 <?php $__env->startSection('content'); ?>
-    
+<main>
+    <section class="contact_image">
+        <img src="<?php echo e(URL::asset('pictures/Contact_Page.jpg')); ?>" alt="contact_image">
+    </section>
+    <h2 id="contact_title">Contact Us</h2>
+
+    <div class="form_container">
+        <form action="" method="POST" id="form">
+            <label for="name">Name</label><br>
+            <input type="text" id="name" name="name"><br>
+            <label for="email">Email</label><br>
+            <input type="email" id="email" name="email"><br>
+            <label for="message">Message</label><br>
+            <textarea name="message" id="message" cols="22" rows="4"></textarea><br>
+            <div class="containerSubmit"><input type="submit" name="submit_button" id="submit_button" value="Send"></div>
+        </form>
+    </div>
+
+    <section class="images">
+        <img src="<?php echo e(URL::asset('pictures/GOV_Contact_Page.jpg')); ?>" alt="GOV_link" >
+        <img src="<?php echo e(URL::asset('pictures/MeteoLux_Contact Page.jpg')); ?>" alt="MeteoLux">
+        <img src="<?php echo e(URL::asset('pictures/MeteoAlarm_Contact_Page.png')); ?>" alt="MeteoAlarm">
+        <img src="<?php echo e(URL::asset('pictures/CGDIS_Contact_Page.jpg')); ?>" alt="CGDIS">
+        <img src="<?php echo e(URL::asset('pictures/RSS_Contact Page.jpg')); ?>" alt="RSS">
+    </section>
+</main>
+
 <?php $__env->stopSection(); ?>
 
 
