bd0d059ddc7b0383cb2b367dd4a0d5c743b67c7c
Jones40 <john.olivier87@gmail.com>
2021-09-05 17:43:29 +0200

    logout fix update

--- 365-Alert Laravel/resources/views/reset-password.blade.php
+++ 365-Alert Laravel/resources/views/reset-password.blade.php
@@ -9,7 +9,7 @@
 @endsection
 {{-- Main Content Section --}}
 @section('content')
-<meta name="csrf-token" content="{{ csrf_token() }}">s
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <img src="{{URL::asset('../css/logos/house.svg')}}" alt="">
 
 <h3>Reset password</h3>
