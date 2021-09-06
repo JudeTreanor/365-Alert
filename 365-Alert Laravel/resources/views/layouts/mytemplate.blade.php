332f1c84757cc29a977f953e3928f5194dd69aa2
AlexDarii <dariisandu@gmail.com>
2021-09-06 11:19:42 +0200

    finished mainTemplate

--- 365-Alert Laravel/resources/views/layouts/mytemplate.blade.php
+++ 365-Alert Laravel/resources/views/layouts/mytemplate.blade.php
@@ -8,8 +8,10 @@
     <title>@yield('title')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ URL::asset('css/main.css'); }}">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
     @yield('css')
 </head>
 
@@ -17,31 +19,31 @@
     <!-- Header with menu and login/register -->
     <header>
 
-        <img id="action" src="{{URL::asset('pictures/Menu_Colapsed_white.svg')}}" alt="">
+        <img id="action" src="{{ URL::asset('pictures/Menu_Colapsed_white.svg') }}" alt="">
 
-        <h2 id="logo">365 Alert</h2>
+        <h2 id="logo"><a href="{{ route('home') }}">365 Alert</a></h2>
 
-        <img id="user-icon" src="{{URL::asset('pictures/User_WhiteStroke_Header.svg')}}" alt="">
+        <img id="user-icon" src="{{ URL::asset('pictures/User_WhiteStroke_Header.svg') }}" alt="">
 
     </header>
 
 
-        <ul id="navigation">
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('about') }}">About</a></li>
-            <li><a href="{{ route('contact') }}">Contact</a></li>
-            <li><a href="{{ route('alerts') }}">Alerts</a></li>
-            <li><a href="{{ route('procedures') }}">Procedures</a></li>
-            <li><a href="{{ route('terms') }}">Terms</a></li>
-            <li><a href="{{ route('client-settings') }}">Client Settings</a></li>
-            <li><a href="{{ route('admin') }}">Admin</a></li>
-        </ul>
+    <ul id="navigation">
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('about') }}">About</a></li>
+        <li><a href="{{ route('contact') }}">Contact</a></li>
+        <li><a href="{{ route('alerts') }}">Alerts</a></li>
+        <li><a href="{{ route('procedures') }}">Procedures</a></li>
+        <li><a href="{{ route('terms') }}">Terms</a></li>
+        <li><a href="{{ route('client-settings') }}">Client Settings</a></li>
+        <li><a href="{{ route('admin') }}">Admin</a></li>
+    </ul>
 
-        <ul class="register">
-            <button><a href="{{ route('home') }}">My Settings</a></button>
-            <button><a href="{{ route('logout') }}">Log Out</a></button>
-        </ul>
-        @csrf
+    <ul class="register">
+        <button><a href="{{ route('home') }}">My Settings</a></button>
+        <button><a href="{{ route('logout') }}">Log Out</a></button>
+    </ul>
+    @csrf
 
 
 
@@ -50,13 +52,39 @@
         @yield('content')
     </main>
     <footer>
+        <section class="need-help-container">
+            <h2 class="need-help">Need Help?</h2>
+            <section class="help_container">
+                <img src="{{ URL::asset('pictures/CGDIS_3_copie.jpg') }}" id="image_cgdis" alt="help">
+                <article id="icons_container">
+                    <a href="tel">
+                        <div>
+                            <img src="{{ URL::asset('pictures/CGDIS_112_White.SVG') }}" id="icon_number" alt="help">
+                            <p id="number_112">112</p>
+                        </div>
+                    </a>
+                    <a href="WWW.112.PUBLIC.LU">
+                        <div>
+                            <img src="{{ URL::asset('pictures/CGDIS_Website_White.SVG') }}" id="icon_website"
+                                alt="help">
+                            <p id="website"><a href="url" style="color:white">www.112.public.lu</a></p>
+                        </div>
+                    </a>
+                    <div>
+                        <img src="{{ URL::asset('pictures/CGDIS_APP_White.SVG') }}" id="icon_app" alt="help">
+                        <p id="gov_app">GOUVALERT</p>
+                    </div>
+                </article>
+            </section>
+
+        </section>
         <!-- footer with two divs and logo image -->
         <section id="footer">
             <div id="container">
 
-                <img src="{{URL::asset('pictures/Facebook_Footer_white.svg')}}" alt="">
-                <img src="{{URL::asset('pictures/Instagram_Footer_white.svg')}}" alt="">
-                <img src="{{URL::asset('pictures/Twitter_Footer_white.svg')}}" alt="">
+                <img src="{{ URL::asset('pictures/Facebook_Footer_white.svg') }}" alt="">
+                <img src="{{ URL::asset('pictures/Instagram_Footer_white.svg') }}" alt="">
+                <img src="{{ URL::asset('pictures/Twitter_Footer_white.svg') }}" alt="">
 
             </div>
             <ul class="terms">
@@ -73,7 +101,7 @@
     </footer>
     {{-- @include('layouts.nav') --}}
     @yield('scripts')
-    <script src="{{URL::asset('js/template.js')}}"></script>
+    <script src="{{ URL::asset('js/template.js') }}"></script>
 </body>
 
 </html>
