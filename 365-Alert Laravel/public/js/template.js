09ad7312f1b8909adb01581c74edad7d5758b29b
AlexDarii <dariisandu@gmail.com>
2021-09-06 16:24:33 +0200

    finished main css(for real:D)

--- 365-Alert Laravel/public/js/template.js
+++ 365-Alert Laravel/public/js/template.js
@@ -10,16 +10,8 @@ function open() {
 }
 button.addEventListener('click', open);
 
+const navtwo = document.querySelector('#navigation-short');
 
-// // simple template page
-// // variable to select the navigation menu
-// const navTwo = document.querySelector('#nav-two');
-
-// // variable to select the navigation button (hamburguer)
-// const button = document.querySelector('#action')
-
-// // function to toggle the class of opened on the menu to move hide/show it
-// function open() {
-//     navTwo.classList.toggle('opened');
-// }
-// button.addEventListener('click', open);
+function open() {
+    navtwo.classList.toggle('opened');
+}
