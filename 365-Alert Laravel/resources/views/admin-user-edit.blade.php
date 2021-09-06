d16fe4016b90a551fd5837e4a66575a63a5286a5
ElsaFRibeiro <elsafreitasribeiro@gmail.com>
2021-09-06 15:30:11 +0200

    afternoon commit

--- 365-Alert Laravel/resources/views/admin-user-edit.blade.php
+++ 365-Alert Laravel/resources/views/admin-user-edit.blade.php
@@ -11,19 +11,19 @@
 
 {{-- Main Content Section --}}
 @section('content')
-   <form action="" method="post">
+   <form action="" method="post" class="form_container">
     @csrf
-    <label for="username">First Name</label>
+    <label for="username">First Name</label><br>
     <input type="text" name="firstname" placeholder="Username" value="{{$user->firstname}}"><br>
-    <label for="username">Last Name</label>
+    <label for="username">Last Name</label><br>
     <input type="text" name="lastname" placeholder="Username" value="{{$user->lastname}}"><br>
-    <label for="email">Email</label>
+    <label for="email">Email</label><br>
     <input type="email" name="email" placeholder="Email" value="{{$user->email}}"><br>
-    <label for="Phone Number">Phone Number</label>
+    <label for="Phone Number">Phone Number</label><br>
     <input type="number" name="contact" placeholder="Phone number" value="{{$user->contact}}"><br>
-    <label for="Password">Password</label>
+    <label for="Password">Password</label><br>
     <input type="text" name="password" placeholder="Password" value="{{$user->password}}"><br>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" id="submit_btn">
    </form>
 @endsection
 
